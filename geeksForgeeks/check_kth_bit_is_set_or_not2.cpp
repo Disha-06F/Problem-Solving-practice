003d6d6bf3fb4155322098a942701ed08ea3212a
H H Joti <150466181+Disha-06F@users.noreply.github.com>
2024-04-08 14:54:38 +0600

    Create check_kth_bit_is_set_or_not2.cpp
    without using class

---
+++ geeksForgeeks/check_kth_bit_is_set_or_not2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+#define F first
+#define S second
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define Disha ios_base::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+using namespace std;
+int main(){
+    Disha;
+    int n,k;
+    cin>>n>>k;
+    int l=(n>>k);
+    if((l&1)==1){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    
+
+}
